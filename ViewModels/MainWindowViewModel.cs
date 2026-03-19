7d349d4539412659ef109105a4ef8ad1585f24e6
JustTom <tomasekzavrel@gmail.com>
2026-03-19 18:21:50 +0100

    Initial Commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace FitnessApp.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace FitnessApp.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
